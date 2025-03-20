d7c5d467d300d86f8cf842c59631d3b141006218
Acer <Acer@LAPTOP-PCP2KOLQ>
2025-03-20 15:59:32 +0800

    added ApplicantDAO

---
+++ src/dao/ApplicantsDAO.java
@@ -0,0 +1,41 @@
+package dao;
+
+import adt.*;
+import entity.Applicant;
+import java.io.*;
+
+
+public class ApplicantsDAO {
+  private String fileName = ""; // For security and maintainability, should not have filename hardcoded here.
+  
+  public void saveToFile(ListInterface<Product> productList) {
+    File file = new File(fileName);
+    try {
+      ObjectOutputStream ooStream = new ObjectOutputStream(new FileOutputStream(file));
+      ooStream.writeObject(productList);
+      ooStream.close();
+    } catch (FileNotFoundException ex) {
+      System.out.println("\nFile not found");
+    } catch (IOException ex) {
+      System.out.println("\nCannot save to file");
+    }
+  }
+
+  public ListInterface<Product> retrieveFromFile() {
+    File file = new File(fileName);
+    ListInterface<Product> productList = new ArrayList<>();
+    try {
+      ObjectInputStream oiStream = new ObjectInputStream(new FileInputStream(file));
+      productList = (ArrayList<Product>) (oiStream.readObject());
+      oiStream.close();
+    } catch (FileNotFoundException ex) {
+      System.out.println("\nNo such file.");
+    } catch (IOException ex) {
+      System.out.println("\nCannot read from file.");
+    } catch (ClassNotFoundException ex) {
+      System.out.println("\nClass not found.");
+    } finally {
+      return productList;
+    }
+  }
+}
