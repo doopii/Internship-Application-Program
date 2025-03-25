5cfd11d6665ae0e3aed81a80737cbc350afc3626
Acer <Acer@LAPTOP-PCP2KOLQ>
2025-03-25 19:32:41 +0800

    entity-interview

---
+++ src/entity/Interview.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Acer
+ */
+public class Interview {
+    private String jobID;
+    private String scheduleID;
+    private String interviewID;
+    private String interviewPosition;
+    private String interviewDate;
+    
+    public Interview(String jobID, String scheduleID, String interviewID, String interviewPosition, String interviewDate) {
+        this.jobID = jobID;
+        this.scheduleID = scheduleID;
+        this.interviewID = interviewID;
+        this.interviewPosition = interviewPosition;
+        this.interviewDate = interviewDate;
+    }
+
+    public String getJobID() {
+        return jobID;
+    }
+
+    public void setJobID(String jobID) {
+        this.jobID = jobID;
+    }
+
+    public String getScheduleID() {
+        return scheduleID;
+    }
+
+    public void setScheduleID(String scheduleID) {
+        this.scheduleID = scheduleID;
+    }
+
+    public String getInterviewID() {
+        return interviewID;
+    }
+
+    public void setInterviewID(String interviewID) {
+        this.interviewID = interviewID;
+    }
+
+    public String getInterviewPosition() {
+        return interviewPosition;
+    }
+
+    public void setInterviewPosition(String interviewPosition) {
+        this.interviewPosition = interviewPosition;
+    }
+
+    public String getInterviewDate() {
+        return interviewDate;
+    }
+
+    public void setInterviewDate(String interviewDate) {
+        this.interviewDate = interviewDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Interview{" + "jobID=" + jobID + ", scheduleID=" + scheduleID + ", interviewID=" + interviewID + ", interviewPosition=" + interviewPosition + ", interviewDate=" + interviewDate + '}';
+    }
+    
+}
